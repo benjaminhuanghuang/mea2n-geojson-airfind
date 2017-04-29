a30bf9040e5f75191dfecb2f1b4648cb17cf147a
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-04-29 09:33:20 -0700

    update

--- models/airport.js
+++ models/airport.js
@@ -32,22 +32,22 @@ module.exports.getAirports = (callback, limit) => {
 
 // Get airports by state
 module.exports.getAirportsByState = (stateCode, callback, limit) => {
-    const query = {
-        loc: {
-            $geoWithin: {
-                $geometry: state.loc
-            }
-        }
-    };
-    const projection = {
-        name: 1,
-        type: 1,
-        code: 1,
-        _id: 0
-    };
     State.findOne({
         code: stateCode
     }, (err, state) => {
+        const query = {
+            loc: {
+                $geoWithin: {
+                    $geometry: state.loc
+                }
+            }
+        };
+        const projection = {
+            name: 1,
+            type: 1,
+            code: 1,
+            _id: 0
+        };
         Airport.find(query, projection, callback).limit(limit).sort([
             ['name', 'ascending']
         ]);
